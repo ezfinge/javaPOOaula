7c7f7c0fa98c64af4f4926efcdc081b506abe720
ezfinge <mirciobrandao@gmail.com>
2025-10-31 18:41:25 -0300

    comentarios novos

--- src/Main.java
+++ src/Main.java
@@ -18,5 +18,39 @@ public class Main {
                         """filme de sessao da tarde, pastelao""" + anoDElancamento;
         System.out.printf(sinopse);
 
+        /**
+             * % seguido de uma letra que indica o tipo de
+             * dado que será inserido no placeholder.
+             * Por exemplo, %s indica que uma String será
+             * inserida no placeholder, %d indica um valor inteiro
+             * e %f indica um valor de ponto flutuante.
+             * Vamos ver um exemplo:
+
+                 String nome = "Maria";
+                 int idade = 30;
+                 double valor = 55.9999;
+                 System.out.println(String.format(
+                    "Meu nome é %s, eu tenho %d anos e
+                    hoje gastei %.2f reais", nome, idade, valor));
+
+         */
+
+        /**
+         * String.format também pode ser usado com Text Block
+         *
+         *
+         *String nome = "João";
+         *int aulas = 4;
+         *
+         * String mensagem = """
+         *                   Olá, %s!
+         *                   Boas vindas ao curso de Java.
+         *                   Teremos %d aulas para te mostrar o que é preciso para você dar o seu primeiro mergulho na linguagem!
+         *                   """.formatted(nome, aulas);
+         *
+         * System.out.println(mensagem);
+         *
+         */
+        
     }
 }
