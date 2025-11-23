57fc7bf5e599da66c1dc5d753e6e0c86c045e066
ezfinge <mirciobrandao@gmail.com>
2025-11-23 15:18:03 -0300

    calculo fatorial

---
+++ src/Fatorial.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Fatorial {
+    public static void main(String[] args) {
+        long resultadoFatorial = 1;
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("impar ou par. digite um valo inteiro. ex:1");
+        System.out.println("pressione a tecla Enter para confirmar");
+        int valor1 = leitura.nextInt();
+
+
+
+        for (int cont = 1; cont <= valor1; cont++){
+            resultadoFatorial = resultadoFatorial * cont;
+        }
+
+        System.out.println("o fatorial de "+valor1+" é "+resultadoFatorial);
+
+    }
+}
